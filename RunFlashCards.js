a3fc791ec03b39b4db070d2cc9134adeaa80c1f2
Mary Morvan <mamorvan@yahoo.com>
2017-05-01 16:12:43 -0400

    user can make custom flash card sets :octopus:

--- RunFlashCards.js
+++ RunFlashCards.js
@@ -72,109 +72,218 @@ var askClozeFlashcards = function(cardSet) {
 inquirer.prompt([
 	{
 	name: "topic",
-	message: "What topic do you want to practice?",
+	message: "Which artist do you want to practice?",
 	type: "list",
-	choices: ["The Smiths", "Leonard Cohen"]
-	},
-	{
-	name: "type",
-	message: "Which type of flashcard would you like?",
-	type: "list",
-	choices: ["basic", "cloze"]	
+	choices: ["The Smiths", "Leonard Cohen", "I want to make my own set!"]
 	}
-]).then(function(flashcardType) {
-	if (flashcardType.topic === "The Smiths") {
-		if(flashcardType.type === "basic") {
-			//add cards to basicSmithSongs set
-			var basicSmithSongs = new CardSetCreator("basic");
-
-			basicSmithSongs.addBasicCard(
-				"\nSweetness, sweetness, I was only joking\n", 
-				"Bigmouth Strikes Again");
-			basicSmithSongs.addBasicCard(
-				"\nCall me morbid, call me pale,\nI spent six years on your trail\n", 
-				"Half a Person");
-			basicSmithSongs.addBasicCard(
-				"\nI am the son and the heir,\nof a shyness that is criminally vulgar\n", 
-				"How Soon Is Now");
-			basicSmithSongs.addBasicCard(
-				"\nNow eighteen years hard labor sounds fair enough\n", 
-				"I Started Something I Couldn't Finish");
-			basicSmithSongs.addBasicCard(
-				"\nUnruly girls,\nWho will not settle down,\nThey must be taken in hand\n", 
-				"Barbarism Begins at Home");
-
-			askBasicFlashcards(basicSmithSongs.cardSet);
-		}
-		else {
-			//add cards to clozeSmithSongs set
-			var clozeSmithSongs = new CardSetCreator("cloze");
-
-			clozeSmithSongs.addClozeCard(
-				"\nFifteen minutes with you,\nWell I wouldn't say no\n", 
-				"say no");
-			clozeSmithSongs.addClozeCard(
-				"\nIf a double-decker bus crashes into us,\nTo die by your side is such a heavenly way to die\n", 
-				"such a heavenly way to die");
-			clozeSmithSongs.addClozeCard(
-				"\nShoplifters of the World,\nUnite and take over\n", 
-				"and take over");
-			clozeSmithSongs.addClozeCard(
-				"\nThere's too much caffeine in your bloodstream,\nAnd a lack of real spice in your life\n", 
-				"of real spice in your life");
-			clozeSmithSongs.addClozeCard(
-				"\nHow can someone so young,\nSing words so sad?\n", 
-				"words so sad");
-
-			askClozeFlashcards(clozeSmithSongs.cardSet);
-		}
-	}
-	else if (flashcardType.topic === "Leonard Cohen") {
-		if(flashcardType.type === "basic") {
-			//add cards to basicLeonardCohen set
-			var basicLeonardCohen = new CardSetCreator("basic");
-
-			basicLeonardCohen.addBasicCard(
-				"\nSo let me judge your love affair,\nIn this room where I have sentenced mine to death\n", 
-				"Take This Longing");
-			basicLeonardCohen.addBasicCard(
-				"\nAnd you know that she's half-crazy but that's why you want to be there,\nAnd she feeds you tea and oranges that come all the way from China\n", 
-				"Suzanne");
-			basicLeonardCohen.addBasicCard(
-				"\nWhat can I possibly say?,\nI guess that I miss you,\nI guess I forgive you\nI'm glad that you stood in my way\n", 
-				"Famous Blue Raincoat");
-			basicLeonardCohen.addBasicCard(
-				"\nYou say you've gone away from me\nBut I can feel you when you breathe\n", 
-				"Avalanche");
-			basicLeonardCohen.addBasicCard(
-				"\nWe met when we were almost young\nDeep in the green lilac parc\n", 
-				"So Long, Marianne");
-
-			askBasicFlashcards(basicLeonardCohen.cardSet);
-		}
-		else {
-			//add cards to clozeLeonardCohen set
-			var clozeLeonardCohen = new CardSetCreator("cloze");
-
-			clozeLeonardCohen.addClozeCard(
-				"\nRing the bells that still can ring,\nForget your perfect offering\nThere is a crack in everything\nThat's how the light gets in\n", 
-				"the light gets in");
-			clozeLeonardCohen.addClozeCard(
-				"\nEverybody knows the fight was fixed,\nThe poor stay poor, the rich get rich,\nThat's how it goes,\nEverybody knows\n", 
-				"Everybody knows");
-			clozeLeonardCohen.addClozeCard(
-				"\nLove is not a victory march,\nIt's a cold and it's a broken hallelujah\n", 
-				"broken hallelujah");
-			clozeLeonardCohen.addClozeCard(
-				"\nYou fixed yourself,\nYou said 'Well, nevermind,\nWe are ugly but we have the music\n", 
-				"we have the music");
-			clozeLeonardCohen.addClozeCard(
-				"\nDance me through the curtains that our kisses have outworn,\nRaise a tent of shelter now, though everything is torn\nDance me to the end of love\n", 
-				"the end of love");
-			askClozeFlashcards(clozeLeonardCohen.cardSet);
-		}
-	}
-	//leave possibility for other topics to be added later
 	
-});
+]).then(function(topic) {
+	//-----------if user chooses Smiths
+	if (topic.topic === "The Smiths") {
+		inquirer.prompt([
+			{
+			name: "type",
+			message: "Which type do you want to study?",
+			type: "list",
+			choices: ["basic", "cloze"]
+			}
+			]).then(function(type) {
 
+				if(type.type === "basic") {
+					//add cards to basicSmithSongs set
+					var basicSmithSongs = new CardSetCreator("basic");
+
+					basicSmithSongs.addBasicCard(
+						"\nSweetness, sweetness, I was only joking\n", 
+						"Bigmouth Strikes Again");
+					basicSmithSongs.addBasicCard(
+						"\nCall me morbid, call me pale,\nI spent six years on your trail\n", 
+						"Half a Person");
+					basicSmithSongs.addBasicCard(
+						"\nI am the son and the heir,\nof a shyness that is criminally vulgar\n", 
+						"How Soon Is Now");
+					basicSmithSongs.addBasicCard(
+						"\nNow eighteen years hard labor sounds fair enough\n", 
+						"I Started Something I Couldn't Finish");
+					basicSmithSongs.addBasicCard(
+						"\nUnruly girls,\nWho will not settle down,\nThey must be taken in hand\n", 
+						"Barbarism Begins at Home");
+					//and run the ask function to display cards
+					askBasicFlashcards(basicSmithSongs.cardSet);
+				}  //else type is not basic therefore cloze
+				else {
+					//add cards to clozeSmithSongs set
+					var clozeSmithSongs = new CardSetCreator("cloze");
+
+					clozeSmithSongs.addClozeCard(
+						"\nFifteen minutes with you,\nWell I wouldn't say no\n", 
+						"say no");
+					clozeSmithSongs.addClozeCard(
+						"\nIf a double-decker bus crashes into us,\nTo die by your side is such a heavenly way to die\n", 
+						"such a heavenly way to die");
+					clozeSmithSongs.addClozeCard(
+						"\nShoplifters of the World,\nUnite and take over\n", 
+						"and take over");
+					clozeSmithSongs.addClozeCard(
+						"\nThere's too much caffeine in your bloodstream,\nAnd a lack of real spice in your life\n", 
+						"of real spice in your life");
+					clozeSmithSongs.addClozeCard(
+						"\nHow can someone so young,\nSing words so sad?\n", 
+						"words so sad");
+					//run the ask function to display cards
+					askClozeFlashcards(clozeSmithSongs.cardSet);
+				} 
+			})//end of .then type
+	}//end of if topic is Smiths
+
+	//-----------if user chooses Leonard Cohen
+	else if (topic.topic === "Leonard Cohen") {
+		inquirer.prompt([
+			{
+			name: "type",
+			message: "Which type do you want to study?",
+			type: "list",
+			choices: ["basic", "cloze"]
+			}
+		]).then(function(type) {
+			if(type.type === "basic") {
+
+				//add cards to basicLeonardCohen set
+				var basicLeonardCohen = new CardSetCreator("basic");
+
+				basicLeonardCohen.addBasicCard(
+					"\nSo let me judge your love affair,\nIn this room where I have sentenced mine to death\n", 
+					"Take This Longing");
+				basicLeonardCohen.addBasicCard(
+					"\nAnd you know that she's half-crazy but that's why you want to be there,\nAnd she feeds you tea and oranges that come all the way from China\n", 
+					"Suzanne");
+				basicLeonardCohen.addBasicCard(
+					"\nWhat can I possibly say?,\nI guess that I miss you,\nI guess I forgive you\nI'm glad that you stood in my way\n", 
+					"Famous Blue Raincoat");
+				basicLeonardCohen.addBasicCard(
+					"\nYou say you've gone away from me\nBut I can feel you when you breathe\n", 
+					"Avalanche");
+				basicLeonardCohen.addBasicCard(
+					"\nWe met when we were almost young\nDeep in the green lilac parc\n", 
+					"So Long, Marianne");
+				//run ask function to display cards
+				askBasicFlashcards(basicLeonardCohen.cardSet);
+			}
+			else { //else type is not basic therefore cloze
+				//add cards to clozeLeonardCohen set
+				var clozeLeonardCohen = new CardSetCreator("cloze");
+
+				clozeLeonardCohen.addClozeCard(
+					"\nRing the bells that still can ring,\nForget your perfect offering\nThere is a crack in everything\nThat's how the light gets in\n", 
+					"the light gets in");
+				clozeLeonardCohen.addClozeCard(
+					"\nEverybody knows the fight was fixed,\nThe poor stay poor, the rich get rich,\nThat's how it goes,\nEverybody knows\n", 
+					"Everybody knows");
+				clozeLeonardCohen.addClozeCard(
+					"\nLove is not a victory march,\nIt's a cold and it's a broken hallelujah\n", 
+					"broken hallelujah");
+				clozeLeonardCohen.addClozeCard(
+					"\nYou fixed yourself,\nYou said 'Well, nevermind,\nWe are ugly but we have the music\n", 
+					"we have the music");
+				clozeLeonardCohen.addClozeCard(
+					"\nDance me through the curtains that our kisses have outworn,\nRaise a tent of shelter now, though everything is torn\nDance me to the end of love\n", 
+					"the end of love");
+				//run ask function to display cards
+				askClozeFlashcards(clozeLeonardCohen.cardSet);
+			}
+		})//end of .then type
+	}//end of if topic is leonard cohen
+
+	//-----------if user chooses to make custom card set
+	else if (topic.topic === "I want to make my own set!") {
+		//get card set name and type
+		inquirer.prompt([
+			{
+			name: "cardSetName",
+			message: "Name your flashcard set"
+			},
+			{
+			name: "type",
+			message: "Which type do you want to make?",
+			type: "list",
+			choices: ["basic", "cloze"]
+			}
+		]).then(function(custom) { 
+			if(custom.type === "basic") {
+				custom.cardSetName = new CardSetCreator("basic");
+				var createCustomCardSet = function() { 
+					//get card content
+					inquirer.prompt([
+						{
+						name: "front",
+						message: "What do you want on the front of your card?"
+						},
+						{
+						name: "back",
+						message: "What do you want on the back of your card?"
+						}
+					]).then(function(customContent) {
+						//add card to custom set
+						custom.cardSetName.addBasicCard(customContent.front, customContent.back);
+						//add cards until user confirms he/she is done
+						inquirer.prompt([
+							{
+							name: "done",
+							message: "Are you finished adding cards?",
+							type: "confirm"
+							}
+						]).then(function(done){
+							if (done.done) {
+								//if done creating cards, run ask function to display set
+								askBasicFlashcards(custom.cardSetName.cardSet);
+							}
+							else {
+								//keep creating cards and adding to custom set
+								createCustomCardSet();
+							}
+						});//end of .then done confirm
+					});//end of .then custom basic cards
+				}//end of createCustomCardSet function
+				//call first create function to start of
+				createCustomCardSet();
+			} 
+			else {
+				custom.cardSetName = new CardSetCreator("cloze");
+				var createCustomCardSet = function() { 
+					//get card content
+					inquirer.prompt([
+						{
+						name: "full",
+						message: "What is your full text?"
+						},
+						{
+						name: "cloze",
+						message: "Which part do you want you want to take out?"
+						}
+					]).then(function(customContent) {
+						//add card to custom set
+						custom.cardSetName.addClozeCard(customContent.full, customContent.cloze);
+						//add cards until user confirms he/she is done
+						inquirer.prompt([
+							{
+							name: "done",
+							message: "Are you finished adding cards?",
+							type: "confirm"
+							}
+						]).then(function(done){
+							if (done.done) {
+								askClozeFlashcards(custom.cardSetName.cardSet);
+							}
+							else {
+								createCustomCardSet();
+							}
+						});//end of .then done or not
+					});//end of .then add custom basic cards
+				};//end of createCustomCardSet function
+				//call first create function to start of
+				createCustomCardSet();
+			}//end of else type is not basic
+		}); //end of .then name set and choose type	
+	} //end if i want to make my own set
+
+}); //end of first inquirer prompt - finally!
